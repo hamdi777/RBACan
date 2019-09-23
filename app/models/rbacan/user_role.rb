9c564fe98f35779c35b461c619f55da9d259f0a3
hamdi <hamdi_amiche@outlook.fr>
2019-09-23 14:42:37 +0100

    validates uniqueness of role

--- app/models/rbacan/user_role.rb
+++ app/models/rbacan/user_role.rb
@@ -1,6 +1,8 @@
 module Rbacan
     class UserRole < ApplicationRecord
         self.table_name = Rbacan.user_role_table
+
+        validates_uniqueness_of :user, scope: :role
         belongs_to :role, class_name: Rbacan.role_class
         belongs_to :user, class_name: Rbacan.permittable_class
     end
