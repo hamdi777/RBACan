0c5369ef38509191db97caaff7ad51768a7f628a
hamdi <hamdi_amiche@outlook.fr>
2019-09-23 15:03:57 +0100

    find or initialize by roles

--- app/models/rbacan/user_role.rb
+++ app/models/rbacan/user_role.rb
@@ -2,7 +2,6 @@ module Rbacan
     class UserRole < ApplicationRecord
         self.table_name = Rbacan.user_role_table
 
-        validates_uniqueness_of :user, scope: :role
         belongs_to :role, class_name: Rbacan.role_class
         belongs_to :user, class_name: Rbacan.permittable_class
     end
